e0b891affe757a3caac3ccfd2faf0f0cce932881
mateussantanasilva <santanasilva1778@gmail.com>
2024-06-13 20:11:57 -0300

    🐛fix: Add host config

--- financial-api/src/server.ts
+++ financial-api/src/server.ts
@@ -1,4 +1,6 @@
 import { app } from './app'
 import { env } from './env'
 
-app.listen({ port: env.PORT }).then(() => console.log('HTTP server running...'))
+app
+  .listen({ port: env.PORT, host: '0.0.0.0' })
+  .then(() => console.log('HTTP server running...'))
