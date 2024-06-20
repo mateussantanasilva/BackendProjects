2eabbcf52a26f1367229f82e073caed1064b62d3
mateussantanasilva <santanasilva1778@gmail.com>
2024-06-20 09:59:44 -0300

    ✨feat: Criar casos de uso da aplicação; definir instâncias de erros

---
+++ gym-pass-api/src/use-cases/check-in.ts
@@ -0,0 +1,67 @@
+import { CheckIn } from '@prisma/client'
+import { CheckInsRespository } from '@/repositories/check-ins-repository'
+import { GymsRepository } from '@/repositories/gyms-repository'
+import { ResourceNotFoundError } from './erros/resource-not-found-error'
+import { getDistanceBetweenCoordinates } from '@/utils/get-distance-between-coodinates'
+import { MaxDistanceError } from './erros/max-distance-error'
+import { MaxNumberOfCheckInsError } from './erros/max-number-of-check-ins-error'
+
+interface CheckInUseCaseRequest {
+  userId: string
+  gymId: string
+  userLatitude: number
+  userLongitude: number
+}
+
+interface CheckInUseCaseResponse {
+  checkIn: CheckIn
+}
+
+export class CheckInUseCase {
+  constructor(
+    private checkInsRepository: CheckInsRespository,
+    private gymsRepository: GymsRepository,
+  ) {}
+
+  async execute({
+    userId,
+    gymId,
+    userLatitude,
+    userLongitude,
+  }: CheckInUseCaseRequest): Promise<CheckInUseCaseResponse> {
+    const gym = await this.gymsRepository.findById(gymId)
+
+    if (!gym) throw new ResourceNotFoundError()
+
+    const distance = getDistanceBetweenCoordinates(
+      {
+        latitude: userLatitude,
+        longitude: userLongitude,
+      },
+      {
+        latitude: gym.latitude.toNumber(),
+        longitude: gym.longitude.toNumber(),
+      },
+    )
+
+    const MAX_DISTANCE_IN_KILOMETERS = 0.1
+
+    if (distance > MAX_DISTANCE_IN_KILOMETERS) throw new MaxDistanceError()
+
+    const checkInOnSameDate = await this.checkInsRepository.findByUserIdOnDate(
+      userId,
+      new Date(),
+    )
+
+    if (checkInOnSameDate) throw new MaxNumberOfCheckInsError()
+
+    const checkIn = await this.checkInsRepository.create({
+      user_id: userId,
+      gym_id: gymId,
+    })
+
+    return {
+      checkIn,
+    }
+  }
+}
---
+++ gym-pass-api/src/use-cases/erros/max-distance-error.ts
@@ -0,0 +1,5 @@
+export class MaxDistanceError extends Error {
+  constructor() {
+    super('Maximum distance of 100 meters reached')
+  }
+}
